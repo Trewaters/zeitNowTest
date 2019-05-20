aa9a15b6e56831918ca6ebe82a243f9fc7f391c7
Tre' Grisby <trewaters@hotmail.com>
2019-05-20 16:10:33 -0700

    after tutorial

---
+++ microservice/monorepo/www/pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        www
+      </h1>
+      <h2 class="subtitle">
+        My premium Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >GitHub</a>
+      </div>
+      <a v-on:click="loadBands">Load Bands</a>
+<ul v-bind:key="band.name" v-for="band in bands">
+  <li>{{ band.name }} - {{ band.genre }}</li>
+</ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  data: function () {
+    return {
+      bands: []
+    }
+  },
+  methods: {
+    async loadBands () {
+      try {
+        this.bands = await this.$axios.$get('/api/bands.js')
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
